aa160110efaed46f3578f4fba714a93ddbca97c2
Benedict Graf <benedict.graf@gmail.com>
2018-06-27 15:37:09 +0200

    V2

--- script.js
+++ script.js
@@ -1,52 +1,90 @@
 //create firebase reference
 var dbRef = new Firebase("https://ng-fire-try.firebaseio.com/");
-var contactsRef = dbRef.child('contacts')
+var contactsRef = dbRef.child('contacts');
 
+/*
+$(document).ready(function() {
+  $("div").click(
+    function(event) {
+      event.preventDefault();
+      var elementURL = $(".linkprev").find("a").attr("href");;
+      console.log(elementURL);
+      $.colorbox({iframe: true, href: elementURL, innerWidth: 645, innerHeight: 509});
+    });
+});
+*/
 
-//load all contacts
-contactsRef.on("child_added", function(snap) {
+//load all contacts (limited to last 20 items)
+contactsRef.limitToLast(20).on("child_added", function(snap) {
   snap.forEach(function(childSnapshot) {
-      var key = childSnapshot.key();
-      var childData = childSnapshot.val();
+  var key = childSnapshot.key();
+  var childData = childSnapshot.val();
 
   //create divs from database-elements
   var card = document.createElement('div');
   card.setAttribute('class', 'linkprev');
-  card.setAttribute('id', 'all');
-  $('#content-list').prepend($(card));
+  $('#content').prepend($(card));
   
   var cardtitle = document.createElement('div');
   cardtitle.setAttribute('class', 'cardtitle');
   cardtitle.innerHTML = childData;
   card.appendChild(cardtitle);
-  
- var y =  document.getElementsByClassName("linkprev")
-for( var i = 0; i < y.length; i++){
- document.guteUrls.execute('')};
-
   });
+  $(document).ready(function(){
+  document.guteUrls.execute('linkprev');
 });
+});
+/*
+$(document.gute-url.onClick).colorbox({rel: 'gal', title: function(){
+  var url = $(this).attr('href');
+  return '<a href="' + url + '" target="_blank">Open In New Window</a>';
+}});
+*/
 
-  
 //change value of element with ID "filter" - checkbox
-function check() {
-  document.getElementById("filter").checked = true;
-}
-
-function uncheck() {
-  document.getElementById("filter").checked = false;
-}
+function check() {document.getElementById("filter").checked = true;}
+function uncheck() {document.getElementById("filter").checked = false;}
 
 //save contact
   //save in database contacts
-document.querySelector(".addValue").addEventListener("click", function( event ) {  
+  document.querySelector(".addValue").addEventListener("click", function( event ) {  
   event.preventDefault();
-  if( document.querySelector('#url').value != '' && document.querySelector('#url').value.charAt(0) == "h" && document.querySelector('#url').value.charAt(3) == "p" && document.querySelector('#f1').checked == true){
-    contactsRef.push({
-        name: document.querySelector('#url').value,})
-      contactForm.reset();}
+    
+  //url validation from inputfield using Regex
+    var valpattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  
+  var valselect = document.getElementById('url');
 
+  var val = valpattern.test(valselect.value);
+  
+  //save in database contacts
+  if(val){
+    contactsRef.push({
+    name: document.querySelector('#url').value,})
+    contactForm.reset();}
+  /*
+  //save in database contacts
+  else if(document.getElementById('url').value.charAt(0) == "h" && document.getElementById('url').value.charAt(3) == "p" && document.querySelector('#f2').checked){
+  contactsRef.push({
+  name: document.querySelector('#url').value,})
+  contactForm.reset();} 
+  //save in database contacts
+  else if(document.getElementById('url').value.charAt(0) == "h" && document.getElementById('url').value.charAt(3) == "p" && document.querySelector('#f3').checked){
+  contactsRef.push({
+  name: document.querySelector('#url').value,})
+  contactForm.reset();} 
+  //save in database contacts
+  else if(document.getElementById('url').value.charAt(0) == "h" && document.getElementById('url').value.charAt(3) == "p" && document.querySelector('#f3').checked){
+  contactsRef.push({
+  name: document.querySelector('#url').value,})
+  contactForm.reset();} 
+  */
   else {
-    alert('Oops, seems like an error with the url-format. Just paste in from your browser with the "http" or "https"');
-  }
+  alert('Oops');}
 }, false);
+  
