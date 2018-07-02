c41951ad8078da177cb2d9af488b757bd3fa04fc
Benedict Graf <benedict.graf@gmail.com>
2018-07-02 14:23:13 +0200

    Cleanedup Code

--- js/script.js
+++ js/script.js
@@ -1,117 +1,85 @@
-//Change all var to let
+//Change all let to let
 //Change forLoops to for of
 
-var invRef = database.ref('innovation');
-var desRef = database.ref('design');
-var tecRef = database.ref('tech');
-var worRef = database.ref('work');
-var ref = [invRef, desRef, tecRef, worRef];
-for(var j = 0; j < ref.length; j++)
+let invRef = database.ref('innovation');
+let desRef = database.ref('design');
+let tecRef = database.ref('tech');
+let worRef = database.ref('work');
+let ref = [invRef, desRef, tecRef, worRef];
+for(let j = 0; j < ref.length; j++)
 
-ref[j].limitToLast(20).on('child_added', function(snap) {
-  //console.log(snapshot.val());
-snap.forEach(function (childSnapshot) {
-  var childData = childSnapshot.val();
-  var card = document.createElement('a');
-  card.setAttribute('class', 'linkprev');
-  card.setAttribute('data-aos', 'fade-up');
-  card.setAttribute('href', this.val  );
-  //card.setAttribute('data-aos-duration', '3000');
-  card.setAttribute('data-aos-easing','ease-in-back');
-  //card.setAttribute('data-aos-delay','300');
-  
+ref[j].limitToLast(20).on('child_added', funcSnap = (snap) => {
+  snap.forEach(funcSnapEach = (childSnapshot) => {
+  let childData = childSnapshot.val();
+  let card = document.createElement('a');
+    card.setAttribute('class', 'linkprev');
+    card.setAttribute('data-aos', 'fade-up');
+    card.setAttribute('data-aos-easing','ease-in-back');
+    //card.setAttribute('href', this.val  );
 
   $('#content').prepend($(card));
-
-  var cardtitle = document.createElement('div');
-  cardtitle.setAttribute('class', 'cardtitle');
-  cardtitle.innerHTML = childData;
+  let cardtitle = document.createElement('div');
+    cardtitle.setAttribute('class', 'cardtitle');
+    cardtitle.innerHTML = childData;
   card.appendChild(cardtitle);
-
   });
  
-  $(document).ready(function () {
+  $(document).ready(funcLinkPrev = () => {
     document.guteUrls.execute('linkprev');
   })
 });
 
-	$('a').click(function(){
-    oldFrame = $('href');
-    console.log(oldFrame);
-		oldFrame.remove();
-    newSrc = "https://www.sitepoint.com/community/t/click-link-replace-src-of-iframe-with-link-src-how/17082/5"
-    console.log(newSrc);
-		newFrame = $('<iframe>').attr('src',newSrc).attr('id','mess').attr('width','597').attr('height','602');
-		return false;
-	});
+let txtEmail = document.querySelector('#txtEmail');
+let txtPassword = document.querySelector('#txtPassword');
+let btnLogin = document.querySelector('#btnlogin');
+let btnLogout = document.querySelector('#btnlogout');
+let btnSignUp = document.querySelector('#btnsignup');
 
-/*
-$(window).ajaxComplete(function() {
-  // Run code
-$('body').toggleClass('loaded')});
-*/
-setTimeout( function(){ 
-  $('body').toggleClass('loaded');
-  //$(content).ajaxComplete(console.log("loaded"),$('body').toggleClass('loaded'));  
-}  , 1000 );
-
-var txtEmail = document.getElementById('txtEmail');
-var txtPassword = document.getElementById('txtPassword');
-var btnLogin = document.getElementById('btnlogin');
-var btnLogout = document.getElementById('btnlogout');
-var btnSignUp = document.getElementById('btnsignup');
-
-btnLogin.addEventListener('click', e => {
-  //get email and pass
-  var email = txtEmail.value;
-  var pass = txtPassword.value;
-  var auth = firebase.auth();
-  //log-in
-  var promise = auth.signInWithEmailAndPassword(email, pass);
+  //Add log-in event
+  btnLogin.addEventListener('click', e => {
+  let email = txtEmail.value;
+  let pass = txtPassword.value;
+  let auth = firebase.auth();
+  let promise = auth.signInWithEmailAndPassword(email, pass);
   promise.catch(e = console.log('log-in user'));
 });
 
   //Add sign-up event
   btnSignUp.addEventListener('click', e=> {
-    //get email and pass
-    //TODO: Validate E-Mail
-  var email = txtEmail.value;
-  var pass = txtPassword.value;
-  var auth = firebase.auth();
-  //log-in
-  var promise = auth.createUserWithEmailAndPassword(email, pass);
+  let email = txtEmail.value;
+  let pass = txtPassword.value;
+  let auth = firebase.auth();
+  let promise = auth.createUserWithEmailAndPassword(email, pass);
   promise.catch(e = console.log('creating user')); 
   });
 
   //Add realtime listener
   firebase.auth().onAuthStateChanged(firebaseUser => {
     if(firebaseUser){console.log('Auth State Changed');
+        //Following is fucking crappy -> change!
     btnLogout.style.removeProperty('display:none');
-    //Following is fucking crappy -> change!
     $('.show').attr("style", "display:block;");
     $('#btnlogout').attr("style", "display:block;"); 
     $('#authfield').attr("style", "display:none;");
-    $('#content').removeClass('inactiveLink')
-    
+    $('#content').removeClass('inactiveLink')  
   }
     else{console.log('not logged in/Auth State Changed');    
   } 
-  
   });
-
+  //Add log-out event
   btnLogout.addEventListener('click', e=>{
     firebase.auth().signOut();
+        //Following is fucking crappy -> change!
     $('.show').attr("style", "display:none;");
     $('#authfield').attr("style", "display:block;"); 
     $('#btnlogout').attr("style", "display:none;"); 
     $('#content').addClass('inactiveLink')    
   });
 
-
 function funcShow(_btn) {
-  var ov = $("#Overlay");
-  var pos = $(_btn).offset();
-  var doc = $(document);
+  let ov = $("#Overlay");
+  let pos = $(_btn).offset();
+  let doc = $(document);
   ov.css({
     top: pos.left + 'px',
     width: 0,
@@ -121,83 +89,50 @@ function funcShow(_btn) {
     $("#content").animate({top: '35%'});
 }
 
+funcClose = () => {
+  $("#Overlay").hide("fast");$(".show").show(900) && $("#content").animate({top: '25%'});}
+funcHide = () => {if($("#Overlay")){$(".show").hide("fast")}}
 
-var iframe = document.getElementsByTagName('iframe')[0];
-var url = iframe.src;
-var getData = function (data) {
-    if (data && data.query && data.query.results && data.query.results.resources && data.query.results.resources.content && data.query.results.resources.status == 200) loadHTML(data.query.results.resources.content);
-    else if (data && data.error && data.error.description) loadHTML(data.error.description);
-    else loadHTML('Error: Cannot load ' + url);
-};
-var loadURL = function (src) {
-    url = src;
-    var script = document.createElement('script');
-    script.src = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20data.headers%20where%20url%3D%22' + encodeURIComponent(url) + '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=getData';
-    document.body.appendChild(script);
-};
-var loadHTML = function (html) {
-    iframe.src = 'about:blank';
-    iframe.contentWindow.document.open();
-    iframe.contentWindow.document.write(html.replace(/<head>/i, '<head><base href="' + url + '"><scr' + 'ipt>document.addEventListener("click", function(e) { if(e.target && e.target.nodeName == "A") { e.preventDefault(); parent.loadURL(e.target.href); } });</scr' + 'ipt>'));
-    iframe.contentWindow.document.close();
-}
-
-loadURL(iframe.src);
-
-function funcClose() { $("#Overlay").hide("fast");$(".show").show(900) && $("#content").animate({top: '25%'});
-;}
-//$(".show").show("fast");
-function funcHide() {if($("#Overlay")){$(".show").hide("fast");}}
-
-//save contact
-//save in database contacts
-var elements = $('.f');
-//console.log(elements);
-
-var buttonPressed = function (event) {event.preventDefault()};
-
-
+let buttonPressed = function (event) {event.preventDefault()};
 
 //url validation from inputfield using Regex
-var valpattern = new RegExp('^(http|https)://' + // protocol
+let valpattern = new RegExp('^(http|https)://' + // protocol
   '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
   '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
   '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
   '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
   '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-var valselect = document.getElementById('url');
-//var val = valpattern.test(valselect.value);
-
+let valselect = document.querySelector('url');
+//let val = valpattern.test(valselect.value);
 
 //mail validation from inputfield using Regex
-var mailpattern = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
-var mailvalselect = document.getElementById('txtEmail');
-var mailval = mailpattern.test(mailvalselect.value); 
+let mailpattern = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
+let mailvalselect = document.getElementById('txtEmail');
+let mailval = mailpattern.test(mailvalselect.value); 
 
+//push data to specific location in firebase
 var push = function(j){j.push({name: document.querySelector('#url').value})
 };
 
 for(var y = 0; y < 5; y++) {
-  $(".f"+y).click((function(id) {
-     return function() {
-      var dtcBtn = (id);
-       
-       $("#button").click(console.log(dtcBtn));
-
+  $(".f" + y).click(function(id) {
+    return function() {
+      var dtcBtn = (id); 
+      $("#button").click(console.log(dtcBtn));
 function push(Ref){Ref.push({name: document.querySelector('#url').value})
 contactForm.reset() + funcClose();}
-
       if(dtcBtn === 1){push(invRef)}
       else if(dtcBtn === 2){push(desRef)}
       else if(dtcBtn === 3){push(tecRef)}
       else if(dtcBtn === 4){push(worRef)};
-
      };
-  }(y)));
+  }(y));
 }
 
-
-for (var i = 0; i < elements.length; i++) { elements[i].addEventListener("click", function add(i){}); }
+//Hide loader when site is loaded
+$(window).on('load', hideLoader = () => {
+  console.log("loaded");
+  $('#loader-wrapper').delay(1000).fadeOut("slow")});
 
 
 //Animation on scroll initialization
